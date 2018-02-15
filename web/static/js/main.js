4ce1533e960bf8fe7df9449c31f013c0ccc080a7
Yuuki Furuyama <addsict@gmail.com>
2018-02-16 00:26:48 +0900

    Add super simple keyboard shortcut for navigating umls

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   var isUmlPage = location.pathname.match(/\/umls\/(\d+)/);
   var umlIdInPath = isUmlPage ? isUmlPage[1] : null;
+  var allUmlIds = $('.uml').map(function(){
+    return this.dataset.umlId
+  }).get();
 
   // setup source copy 
   var clipboard = new Clipboard('.uml__modal__body__source__header__copy');
@@ -29,4 +32,31 @@ $(document).ready(function() {
       },
     });
   });
+
+  // keyboard shortcut
+  document.onkeydown = function(e) {
+    if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+      var isUmlPage = location.pathname.match(/\/umls\/(\d+)/);
+      if (!isUmlPage || isUmlPage.length < 2) {
+        return;
+      }
+      var umlId = isUmlPage[1];
+
+      var idx = allUmlIds.indexOf(umlId);
+      if (idx !== -1) {
+        // hide current
+        $('#uml__modal__' + umlId).popup('hide');
+
+        // show previous
+        if (e.key === 'ArrowLeft' && idx > 0) {
+          $('#uml__modal__' + allUmlIds[idx - 1]).popup('show');
+        }
+
+        // show next
+        if (e.key === 'ArrowRight' && allUmlIds.length > idx + 1) {
+          $('#uml__modal__' + allUmlIds[idx + 1]).popup('show');
+        }
+      }
+    }
+  };
 });
